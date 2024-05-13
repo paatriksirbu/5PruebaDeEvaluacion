d95e008be7a74013ee42f8f0c5463977d590a02b
Patrik <psirb@myuax.com>
2024-05-13 15:31:56 +0200

    constructor y metodo para obtener la instancia. Para poder usar el patron Singleton

--- src/main/java/GestionCadenaRestaurantes/PatronesEstructuracion/Manager/RestaurantManager.java
+++ src/main/java/GestionCadenaRestaurantes/PatronesEstructuracion/Manager/RestaurantManager.java
@@ -2,12 +2,23 @@ package GestionCadenaRestaurantes.PatronesEstructuracion.Manager;
 
 import GestionCadenaRestaurantes.PatronesEstructuracion.Restaurant;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class RestaurantManager {
 
     private static RestaurantManager instance;
-    private List<Restaurant> restaurant;
+    private List<Restaurant> restaurants;
 
+    private RestaurantManager() {
+        restaurants = new ArrayList<>();
+    }
+
+    public static RestaurantManager getInstance() {
+        if (instance == null) {
+            instance = new RestaurantManager();
+        }
+        return instance;
+    }
 
 }
