f4d4a14ed81d94934d46b59259adc2a9cba09202
Patrik <psirb@myuax.com>
2024-05-13 15:51:14 +0200

    RestaurantLeaf

--- src/main/java/GestionCadenaRestaurantes/PatronesEstructurales/RestaurantLeaf.java
+++ src/main/java/GestionCadenaRestaurantes/PatronesEstructurales/RestaurantLeaf.java
@@ -1,4 +1,38 @@
 package GestionCadenaRestaurantes.PatronesEstructurales;
 
-public class RestaurantLeaf {
+import GestionCadenaRestaurantes.PatronesEstructurales.Interface.RestaurantComponent;
+
+import java.util.List;
+
+public class RestaurantLeaf implements RestaurantComponent {
+    private String name;
+
+    public RestaurantLeaf(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void prepareTables() {
+        System.out.println("Preparing tables for " + name);
+    }
+
+    @Override
+    public void serveFood() {
+        System.out.println("Serving food for " + name);
+    }
+
+    @Override
+    public void add(RestaurantComponent restaurantComponent) {
+        throw new UnsupportedOperationException("Leaf nodes cannot have children");
+    }
+
+    @Override
+    public void remove(RestaurantComponent restaurantComponent) {
+        throw new UnsupportedOperationException("Leaf nodes cannot remove components.");
+    }
+
+    @Override
+    public List<RestaurantComponent> getChildren() {
+        return null;
+    }
 }
