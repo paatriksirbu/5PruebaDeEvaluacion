2e522a615f36f30859450770f3a883dda25787d6
Patrik <psirb@myuax.com>
2024-05-13 15:30:38 +0200

    Creacion del manager. Gestor de operaciones y listas de restaurantes.

---
+++ src/main/java/GestionCadenaRestaurantes/PatronesEstructuracion/Manager/RestaurantManager.java
@@ -0,0 +1,13 @@
+package GestionCadenaRestaurantes.PatronesEstructuracion.Manager;
+
+import GestionCadenaRestaurantes.PatronesEstructuracion.Restaurant;
+
+import java.util.List;
+
+public class RestaurantManager {
+
+    private static RestaurantManager instance;
+    private List<Restaurant> restaurant;
+
+
+}
