42b5c70e15d846bc25305e3bd1535ba160b7a207
Patrik <psirb@myuax.com>
2024-05-13 15:51:46 +0200

    Restaurant Composite.

--- src/main/java/GestionCadenaRestaurantes/PatronesEstructurales/RestaurantComposite.java
+++ src/main/java/GestionCadenaRestaurantes/PatronesEstructurales/RestaurantComposite.java
@@ -1,4 +1,44 @@
 package GestionCadenaRestaurantes.PatronesEstructurales;
 
-public class RestaurantComposite {
+import GestionCadenaRestaurantes.PatronesEstructurales.Interface.RestaurantComponent;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class RestaurantComposite implements RestaurantComponent {
+    private List<RestaurantComponent> children = new ArrayList<>();
+    private String name;  // Nombre del grupo de restaurantes para identificación
+
+    public RestaurantComposite(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void prepareTables() {
+        for (RestaurantComponent child : children) {
+            child.prepareTables();
+        }
+    }
+
+    @Override
+    public void serveFood() {
+        for (RestaurantComponent child : children) {
+            child.serveFood();
+        }
+    }
+
+    @Override
+    public void add(RestaurantComponent component) {
+        children.add(component);
+    }
+
+    @Override
+    public void remove(RestaurantComponent component) {
+        children.remove(component);
+    }
+
+    @Override
+    public List<RestaurantComponent> getChildren() {
+        return children;
+    }
 }
